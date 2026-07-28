931f642c16797ff58e5b0699352d673371a8d05c
Güney Kıymaç <guneykiymac@gmail.com>
2026-07-28 22:29:55 +0200

    Add header export for order1 simulation

--- SymbolicDSGE/_ckernels/monte_carlo/core_steps.h
+++ SymbolicDSGE/_ckernels/monte_carlo/core_steps.h
@@ -5,4 +5,10 @@
 #include "../core/core.h"
 #include "../kalman/kalman.h"
 
+void sdsge_simulate_order1_step(
+    const f64 *SDSGE_RESTRICT A, const f64 *SDSGE_RESTRICT B,
+    const f64 *SDSGE_RESTRICT C, const f64 *SDSGE_RESTRICT d,
+    const f64 *SDSGE_RESTRICT x0, const f64 *SDSGE_RESTRICT shock, const i64 T,
+    const i64 n, const i64 k, const i64 m, f64 *SDSGE_RESTRICT simout);
+
 #endif /* SDSGE_MC_CORE_STEPS */
